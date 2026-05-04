954a09aeb008250e989b6948dd6c4c409534d269
panic_machine <87824619+VtheSaint@users.noreply.github.com>
2026-05-04 08:12:02 +0300

    fix: evaluation script

--- src/model/api.py
+++ src/model/api.py
@@ -1,11 +1,14 @@
 from src.config._parsers import ModelConfig
 from .resnet import build_resnet_18_10
 
-def get_model(
-        cfg: ModelConfig,
-        device: str,
-):
-    if cfg.name == 'resnet18_10':
-        return build_resnet_18_10(cfg.weights_path, device, num_classes=cfg.num_classes)
+
+def get_model(cfg: ModelConfig, device: str):
+    if cfg.name == "resnet18_10":
+        return build_resnet_18_10(
+            weights_path=cfg.weights_path,
+            device=device,
+            pretrained=cfg.pretrained,
+            num_classes=cfg.num_classes,
+        )
     else:
-        raise Exception("Unknown dataset: {}".format(cfg.name))
+        raise Exception("Unknown model: {}".format(cfg.name))
