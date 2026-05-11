d2f04aa08458fa666fa5a4b8d86bee916aed85ba
panic_machine <velimir.nikolsky@gmail.com>
2026-05-11 20:03:42 +0000

    awp smoothed train

--- cmd/smoothed_awp.sh
+++ cmd/smoothed_awp.sh
@@ -1,2 +1,2 @@
-CONFIG_PATH=${CONFIG_PATH:-configs/train/smoothed-AWP-resnet18-cifar10.yaml}
-python -m src.exp.smoothed_awp --config "${CONFIG_PATH}"
+CONFIG_PATH=configs/train/smoothed-AWP-resnet18-cifar10.yaml
+python -m src.exp.smooth_adv --config "${CONFIG_PATH}"
