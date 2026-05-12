5c73d3071e554b1974e32d187b1f967cc4430db4
panic_machine <87824619+VtheSaint@users.noreply.github.com>
2026-05-12 10:02:32 +0300

    feat: implement input images generator

--- cmd/evaluation.sh
+++ cmd/evaluation.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
- echo "=== Evaluating adversarial PGD training model ==="
- python -m src.exp.evaluate --config configs/eval/adv-PGD-resnet18-cifar10.yaml
+# echo "=== Evaluating adversarial PGD training model ==="
+# python -m src.exp.evaluate --config configs/eval/adv-PGD-resnet18-cifar10.yaml
 
  echo "=== Evaluating adversarial FGSM training model ==="
  python -m src.exp.evaluate --config configs/eval/adv-FGSM-resnet18-cifar10.yaml
 
- echo "=== Evaluating gaussian training model ==="
- python -m src.exp.evaluate --config configs/eval/gaussian-noise-resnet18-cifar10.yaml
+# echo "=== Evaluating gaussian training model ==="
+# python -m src.exp.evaluate --config configs/eval/gaussian-noise-resnet18-cifar10.yaml
 
- echo "=== Evaluating TRADES model ==="
- python -m src.exp.evaluate --config configs/eval/trades-resnet18-cifar10.yaml
+# echo "=== Evaluating TRADES model ==="
+# python -m src.exp.evaluate --config configs/eval/trades-resnet18-cifar10.yaml
 
- echo "=== Evaluating MACER model ==="
- python -m src.exp.evaluate --config configs/eval/macer-resnet18-cifar10.yaml
+# echo "=== Evaluating MACER model ==="
+# python -m src.exp.evaluate --config configs/eval/macer-resnet18-cifar10.yaml
+
+#echo "=== Evaluation baseline model ==="
+#python -m src.exp.evaluate --config configs/eval/evaluation-baseline-resnet18-cifar10.yaml
+
+echo "=== Evaluation Smooth Adv ==="
+python -m src.exp.smooth_adv --config
+
+echo "=== Evalutation Smooth Adv + AWP ==="
+python -m src.exp.awp_smooth_adv --config
+
+echo "=== Evaluation TRADES AWP ==="
+python -m src.exp.evaluate -- config
 
 
-echo "=== Evaluation baseline model ==="
-python -m src.exp.evaluate --config configs/eval/evaluation-baseline-resnet18-cifar10.yaml
 
 echo "=== Done ==="
