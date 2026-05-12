6128228414c86c8c9ca31db1bd4b8d4642eedde4
panic_machine <87824619+VtheSaint@users.noreply.github.com>
2026-05-12 23:37:43 +0300

    add google drive integration, drop git lfs tracking

--- src/config/secret.py
+++ src/config/secret.py
@@ -5,3 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 WANDB_TOKEN = os.getenv("WANDB_TOKEN")
+GDRIVE_FOLDER_ID = os.getenv("GDRIVE_FOLDER_ID")
+GSERVICE_KEY_PATH = os.getenv("GSERVICE_KEY_PATH")
+GOAUTH_KEY_PATH = os.getenv("GOAUTH_KEY_PATH")
